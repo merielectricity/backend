9a9c631189ef6d4158c9720991e508b6e8720ba8
Ranjeet Singh <techonerd1.0@gmail.com>
2022-05-20 18:28:46 +0530

    stagging config

--- solarverse/settings/stagging.py
+++ solarverse/settings/stagging.py
@@ -1,1 +1,22 @@
 from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', ''),
+    }
+}
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_BROWSER_XSS_FILTER = True
