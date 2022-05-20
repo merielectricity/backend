12942d85125a16abc021dbab49c0f43f3200a538
sumit-6 <75926522+sumit-6@users.noreply.github.com>
2022-05-20 22:42:17 +0530

    Update base.py

--- solarverse/settings/base.py
+++ solarverse/settings/base.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from oscar.defaults import *
+from .logging import LOGGING
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -178,3 +179,5 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = LOGGING
