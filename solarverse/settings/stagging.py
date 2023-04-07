760930fd76193588fe974ff0c3563468a48189e5
megamind12 <abhisheksingh1395@gmail.com>
2023-04-08 05:04:29 +0530

    django extensions commented

--- solarverse/settings/stagging.py
+++ solarverse/settings/stagging.py
@@ -7,7 +7,7 @@ DEBUG = os.environ.get("DEBUG_VALUE", False)
 ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS += [
-     "django_extensions",
+     #"django_extensions",
 ]
 
 OSCARAPI_USER_FIELDS = ("first_name","last_name", "email", "phone_number","date_joined","last_login", "is_phone_verified","is_staff","is_active","is_email_verified")
