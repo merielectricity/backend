5278cf59f35acd145f43d3499f08d0867d0f74b6
megamind12 <abhisheksingh1395@gmail.com>
2023-04-07 19:07:30 +0530

    Custom user model

---
+++ customapps/svuser/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'customapps.svuser.apps.SVUserConfig'
---
+++ customapps/svuser/models.py
@@ -0,0 +1,139 @@
+from django.contrib.auth import models as auth_models
+from django.core.mail import send_mail
+from django.core.validators import RegexValidator
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.crypto import get_random_string
+from django.utils.translation import gettext_lazy as _
+
+from oscar.core.compat import AUTH_USER_MODEL
+
+
+PhoneValidator = RegexValidator(
+    regex=r"^\d{10}$", message="phone number must be 10 digits long."
+)
+
+
+class UserManager(auth_models.BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given email and
+        password.
+        """
+        now = timezone.now()
+        if not email:
+            raise ValueError("The given email must be set")
+        email = UserManager.normalize_email(email)
+        user = self.model(
+            email=email,
+            is_staff=False,
+            is_active=True,
+            is_superuser=False,
+            last_login=now,
+            date_joined=now,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        u = self.create_user(email, password, **extra_fields)
+        u.is_staff = True
+        u.is_active = True
+        u.is_superuser = True
+        u.save(using=self._db)
+        return u
+
+
+class SVUser(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
+    """
+    An abstract base user suitable for use in Oscar projects.
+
+    This is basically a copy of the core AbstractUser model but without a
+    username field
+    """
+
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(
+        _("First name"),
+        max_length=255,
+        blank=True,
+    )
+    last_name = models.CharField(_("Last name"), max_length=255, blank=True)
+    phone_number = models.CharField(
+        _("Phone number"),
+        max_length=10,
+        validators=[PhoneValidator],
+        blank=True,
+        unique=True,
+    )
+    is_staff = models.BooleanField(
+        _("Staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin " "site."),
+    )
+    is_active = models.BooleanField(
+        _("Active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as "
+            "active. Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    is_phone_verified = models.BooleanField(_("Phone Number Verified"), default=False)
+    otp = models.CharField(_("Recent OTP"), max_length=6, blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    class Meta:
+        # abstract = True
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """
+        Return the short name for the user.
+        """
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Send an email to this user.
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    def _migrate_alerts_to_user(self):
+        """
+        Transfer any active alerts linked to a user's email address to the
+        newly registered user.
+        """
+        ProductAlert = self.alerts.model
+        alerts = ProductAlert.objects.filter(
+            email=self.email, status=ProductAlert.ACTIVE
+        )
+        alerts.update(user=self, key="", email="")
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        # Migrate any "anonymous" product alerts to the registered user
+        # Ideally, this would be done via a post-save signal. But we can't
+        # use get_user_model to wire up signals to custom user models
+        # see Oscar ticket #1127, Django ticket #19218
+        self._migrate_alerts_to_user()
